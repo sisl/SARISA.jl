c0e60fe84c84358748326d0a64872811c8e6a996
Louis Dressel <dressel@stanford.edu>
2017-02-12 08:56:11 -0800

    better documentation in source code

--- src/SARISA.jl
+++ src/SARISA.jl
@@ -1,24 +1,24 @@
 module SARISA
 
-# package code goes here
 export sarisa
 
-# SARISA
 """
-`sarisa(pomdpx_file, timeout, idx=0; filename="temp.policy")`
+`sarisa(pomdpx_file::String, timeout::Real=30, variant::String="MN"; filename="temp.policy")`
 
-`idx` is the please variant index. If you leave it blank, rho-appl is used.
-filename is the name of the saved policy file. The default is temp.policy.
+The `timeout` is expressed in seconds. There are three main `variant` strings:
+
+* `MN`		Max-norm reward function.
+* `GU`		Guess reward function.
+* `TH_90`	Threshold reward function with cutoff at `||b||_inf = 0.9`.
+
+There are two more allowed `variant` strings, but these use the unimproved lower bound. The only reason they are included is to show the value of using my improved lower bound.
+
+* `MB`		Max-norm with bad (unimproved) lower bound.
+* `TB_90`	Guess reward function with bad (unimproved) lower bound.
+
+The resulting policy file is saved to `filename`.
 """
-function sarisa(pomdpx_file, timeout, idx::Int; filename="temp.policy")
-	if idx == 0
-		sarisa_path = Pkg.dir("SARISA", "deps", "rho-appl", "src", "pomdpsol")
-	else
-		sarisa_path = Pkg.dir("SARISA", "deps", "rho-appl$(idx)", "src", "pomdpsol")
-	end
-	run(`$sarisa_path $(pomdpx_file) --output $(filename) --timeout $timeout`)
-end
-function sarisa(pomdpx_file, timeout, variant::String="MN"; filename="temp.policy")
+function sarisa(pomdpx_file::String, timeout::Real=30, variant::String="MN"; filename="temp.policy")
 
 	exec_name = "pomdpsol_$(variant)"
 	sarisa_path = Pkg.dir("SARISA", "deps", "appl-sarisa", "src", exec_name)
